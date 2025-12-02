b2145b8dadb75d5b5667348d42f0bbfc5a60eb7a
Daniel Godoy <danielgodoy66@gmail.com>
2025-12-02 23:12:42 +0100

    add sort_tuples solution

--- exercises_by_topic/algorithms/sorting.py
+++ exercises_by_topic/algorithms/sorting.py
@@ -19,7 +19,7 @@ def bubble_sort(nums):
     return nums
 
 
-print(bubble_sort([4, 2, 9, 1]))
+print(f"BubbleSort: {bubble_sort([4, 2, 9, 1])}")
 
 
 """
@@ -41,7 +41,7 @@ def selection_sort(nums):
     return nums
 
 
-print(selection_sort([7, 3, 5, 2]))
+print(f"SelectionSort: {selection_sort([7, 3, 5, 2])}")
 
 """
 Ejercicio 3 — QuickSort (recursivo)
@@ -53,18 +53,21 @@ quicksort([3,6,8,2,5])  # [2,3,5,6,8]
 def quicksort(nums):
     if len(nums) <= 1:
         return nums
-    
+
     # get middle number
-    pivot = nums[len(nums)//2]
-    
+    pivot = nums[len(nums) // 2]
+
     # build a list for all the smaller, equal and bigger numbers, comparing with pivot (middle number)
     left = [x for x in nums if x < pivot]
     middle = [x for x in nums if x == pivot]
     right = [x for x in nums if x > pivot]
-    
+
     # recursively sort smaller and bigger numbers.
     # Concatenate the results to return the sorted list with all numbers.
     return quicksort(left) + middle + quicksort(right)
+
+
+print(f"QuickSort: {quicksort([3, 6, 8, 2, 5])}")
 
 """
 Ejercicio 4 — ordenar tuplas por el segundo elemento
@@ -72,3 +75,17 @@ Ejercicio 4 — ordenar tuplas por el segundo elemento
 sort_tuples([(“Alex”,30),(“Bob”,20),(“Carl”,25)])
 # → [("Bob",20),("Carl",25),("Alex",30)]
 """
+
+
+def sort_tuples(tuples):
+    n = len(tuples)
+    for i in range(n):
+        smallest_idx = i
+        for j in range(i + 1, n):
+            if tuples[j][1] < tuples[smallest_idx][1]:
+                smallest_idx = j
+        tuples[i], tuples[smallest_idx] = tuples[smallest_idx], tuples[i]
+    return tuples
+
+
+print(f"SortTuples: {sort_tuples([("Alex", 30), ("Bob", 20), ("Carl", 25)])}")
