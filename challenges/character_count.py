c7cf47de9a7b43e452b4d4f938ff8bd9750952ad
Daniel Godoy <danielgodoy66@gmail.com>
2025-12-28 23:00:54 +0100

    add character_count challenge

---
+++ challenges/character_count.py
@@ -0,0 +1,38 @@
+"""
+Character Count
+Given a sentence string, return an array with a count of each character in alphabetical order.
+
+Treat upper and lowercase letters as the same letter when counting.
+Ignore numbers, spaces, punctuation, etc.
+Return the count and letter in the format "letter count". For instance, "a 3".
+All returned letters should be lowercase.
+Do not return a count of letters that are not in the given string.
+
+count_characters("hello world") should return ["d 1", "e 1", "h 1", "l 3", "o 2", "r 1", "w 1"]
+
+"""
+
+import re
+
+
+def count_characters(sentence):
+    sentence = re.sub("[^A-Za-z]+", "", sentence).lower()
+    letters = {}
+    already_checked = []
+    output = []
+    for i in sentence:
+        for j in sentence:
+            if i.lower() == j.lower() and i not in already_checked:
+                if i in letters:
+                    letters[i] += 1
+                else:
+                    letters[i.lower()] = 1
+        if i not in already_checked:
+            already_checked.append(i.lower())
+    sorted_dict = dict(sorted(letters.items()))
+    for key, value in sorted_dict.items():
+        output.append(key + " " + str(value))
+    return output
+
+
+print(count_characters("I love coding challenges!"))
